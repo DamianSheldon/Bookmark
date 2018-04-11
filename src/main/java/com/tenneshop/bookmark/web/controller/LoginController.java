c74b170c27da65d604472354b12ae6f47790c13c
Meiliang Dong <dongmeilianghy@sina.com>
2018-04-11 15:35:39 +0800

    Added login business logic.

--- src/main/java/com/tenneshop/bookmark/web/controller/LoginController.java
+++ src/main/java/com/tenneshop/bookmark/web/controller/LoginController.java
@@ -1,11 +1,16 @@
 package com.tenneshop.bookmark.web.controller;
 
+import java.util.List;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.context.WebContext;
+
+import com.tenneshop.bookmark.web.util.MysqlJDBCUtil;
 
 public class LoginController implements IController {
 
@@ -13,8 +18,33 @@ public class LoginController implements IController {
 			ITemplateEngine templateEngine) throws Exception {
 		
 		WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-        
-        templateEngine.process("login", ctx, response.getWriter());
+		
+		HttpSession session = request.getSession();
+		
+		// Check username and password
+		String username = request.getParameter("user_name");
+		String password = request.getParameter("user_password");
+		
+		if (username != null && password != null) {
+			// TODO: More strict validate
+			// Login
+			if (MysqlJDBCUtil.getInstance().login(username, password)) {
+				session.setAttribute("valid_user", username);
+				
+				ctx.setVariable("loginUser", username);
+				
+				// Fetch URLs from database
+				List<String> urls = MysqlJDBCUtil.getInstance().getUserUrls(username);
+				ctx.setVariable("urls", urls);
+				
+				templateEngine.process("home", ctx, response.getWriter());
+			}
+			else {
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not log you in.");
+			}
+		}
+		else {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not log you in.");
+		}
 	}
-
 }
--- src/main/java/com/tenneshop/bookmark/web/util/MysqlJDBCUtil.java
+++ src/main/java/com/tenneshop/bookmark/web/util/MysqlJDBCUtil.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,10 +38,13 @@ public class MysqlJDBCUtil {
 	
 	public boolean registerUser(String username, String password, String email) 
 	{
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
 		Connection c = getMysqlDatabaseConnection();
 		if (c != null) {
 			try {
-				PreparedStatement stmt = c.prepareStatement("insert into user value (?, ?, ?);");
+				stmt = c.prepareStatement("insert into user value (?, ?, ?);");
 				stmt.setString(1, username);
 				stmt.setString(2, password);
 				stmt.setString(3, email);
@@ -48,41 +52,141 @@ public class MysqlJDBCUtil {
 				stmt.executeUpdate();
 				
 				return true;	
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return false;	
+			}
+			catch (SQLException ex){
+			    // handle any errors
+			    System.out.println("SQLException: " + ex.getMessage());
+			    System.out.println("SQLState: " + ex.getSQLState());
+			    System.out.println("VendorError: " + ex.getErrorCode());
+			}
+			finally {
+			    // it is a good idea to release
+			    // resources in a finally{} block
+			    // in reverse-order of their creation
+			    // if they are no-longer needed
+
+			    if (rs != null) {
+			        try {
+			            rs.close();
+			        } catch (SQLException sqlEx) { } // ignore
+
+			        rs = null;
+			    }
+
+			    if (stmt != null) {
+			        try {
+			            stmt.close();
+			        } catch (SQLException sqlEx) { } // ignore
+
+			        stmt = null;
+			    }
 			}
 		}
-		else {
-			return false;
-		}
+		return false;
 	}
 	
 	public List<String> getUserUrls(String user)
 	{
 		List<String> urls = new ArrayList<String>();
 		
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
 		//extract from the database all the URLs this user has stored
 		Connection c = getMysqlDatabaseConnection();
 		if (c != null) {
-			PreparedStatement stmt;
 			try {
 				stmt = c.prepareStatement("select bm_URL from bookmark where username = ?;");
 				stmt.setString(1, user);
 				
-				ResultSet rs = stmt.executeQuery();
+				rs = stmt.executeQuery();
 				while (rs.next()) {
 					String url = rs.getString("bm_URL");
 					urls.add(url);
 				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			}
+			catch (SQLException ex){
+			    // handle any errors
+			    System.out.println("SQLException: " + ex.getMessage());
+			    System.out.println("SQLState: " + ex.getSQLState());
+			    System.out.println("VendorError: " + ex.getErrorCode());
+			}
+			finally {
+			    // it is a good idea to release
+			    // resources in a finally{} block
+			    // in reverse-order of their creation
+			    // if they are no-longer needed
+
+			    if (rs != null) {
+			        try {
+			            rs.close();
+			        } catch (SQLException sqlEx) { } // ignore
+
+			        rs = null;
+			    }
+
+			    if (stmt != null) {
+			        try {
+			            stmt.close();
+			        } catch (SQLException sqlEx) { } // ignore
+
+			        stmt = null;
+			    }
 			}
 		}
 		
 		return urls;
+	}
+	
+	public boolean login(String username, String password)
+	{
+		// check if username is unique
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		Connection c = getMysqlDatabaseConnection();
+		if (c != null) {
+			String sql = "select COUNT(*) AS rowcount from user where username = \"" + username + "\" and passwd = \"" + password + "\";";
+			try {
+				stmt = c.createStatement();
+				rs = stmt.executeQuery(sql);
+				rs.next();
+				int count = rs.getInt("rowcount");
+				
+				if (count > 0) {
+					return true;
+				}
+			}
+			catch (SQLException ex){
+			    // handle any errors
+			    System.out.println("SQLException: " + ex.getMessage());
+			    System.out.println("SQLState: " + ex.getSQLState());
+			    System.out.println("VendorError: " + ex.getErrorCode());
+			}
+			finally {
+			    // it is a good idea to release
+			    // resources in a finally{} block
+			    // in reverse-order of their creation
+			    // if they are no-longer needed
+
+			    if (rs != null) {
+			        try {
+			            rs.close();
+			        } catch (SQLException sqlEx) { } // ignore
+
+			        rs = null;
+			    }
+
+			    if (stmt != null) {
+			        try {
+			            stmt.close();
+			        } catch (SQLException sqlEx) { } // ignore
+
+			        stmt = null;
+			    }
+			}
+		}
+		
+		return false;
 	}
 	
 	private Connection getMysqlDatabaseConnection()
